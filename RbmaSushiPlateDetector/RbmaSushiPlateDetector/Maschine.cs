7db7eefca92b056b541328aabba973c6f637ab16
mhl4tky <mhl4tky@gmail.com>
2014-11-03 21:48:21 +0100

    even more cleanup

--- RbmaSushiPlateDetector/RbmaSushiPlateDetector/Maschine.cs
+++ RbmaSushiPlateDetector/RbmaSushiPlateDetector/Maschine.cs
@@ -1,30 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Ni.Libraries.Hardware;
-using Ni.Libraries.Util;
-using OpenCvSharp;
+﻿//using Ni.Libraries.Hardware;
+//using Ni.Libraries.Util;
 
 namespace RbmaSushiPlateDetector
 {
     class Maschine
     {
-        private MaschineMk2 _mk2;
+        //private MaschineMk2 _mk2;
 
         public Maschine()
         {
-            _mk2 = new MaschineMk2();
-            _mk2.SetRestLeds(new int[31].SetAllValues(127));
+            //_mk2 = new MaschineMk2();
+            //_mk2.SetRestLeds(new int[31].SetAllValues(127));
         }
 
         public void SetColor(double hue)
         {
             var c =  Helpers.ConvertHsvToRgb(hue, 1, 1);
 
-            _mk2.SetPadLeds(new int[16].SetAllValues(c.R), new int[16].SetAllValues(c.G), new int[16].SetAllValues(c.B));
-            _mk2.SetGroupLeds(new int[8].SetAllValues(c.R), new int[8].SetAllValues(c.G), new int[8].SetAllValues(c.B), new int[8].SetAllValues(127));
+            //_mk2.SetPadLeds(new int[16].SetAllValues(c.R), new int[16].SetAllValues(c.G), new int[16].SetAllValues(c.B));
+            //_mk2.SetGroupLeds(new int[8].SetAllValues(c.R), new int[8].SetAllValues(c.G), new int[8].SetAllValues(c.B), new int[8].SetAllValues(127));
         }
     }
 }
